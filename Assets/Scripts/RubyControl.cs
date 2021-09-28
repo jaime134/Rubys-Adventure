b9b3218ac9ba275b2045b7986aea3931f8625db3
JoanSF99 <al376128@uji.es>
2021-09-28 16:55:47 +0200

    Coleccionables

--- Assets/Scripts/RubyControl.cs
+++ Assets/Scripts/RubyControl.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class RubyControl : MonoBehaviour
 {
+
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -20,4 +29,11 @@ public class RubyControl : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
