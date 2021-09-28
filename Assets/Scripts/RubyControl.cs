a410c42b21300bb9a363c3a105970b276b9514a1
jaime134 <al394560@uji.es>
2021-09-28 16:56:35 +0200

    ¿Cómo arreglar la inestabilidad de Ruby?

--- Assets/Scripts/RubyControl.cs
+++ Assets/Scripts/RubyControl.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubyControl : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
+   Rigidbody2D rigidbody2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,9 +17,11 @@ public class RubyControl : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
+
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+
+        rigidbody2d.MovePosition(position);
     }
 }
