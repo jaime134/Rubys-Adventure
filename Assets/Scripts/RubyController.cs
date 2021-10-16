66f1b330453cb6bcdf2c7e2020f2da14fceef80d
jaime134 <al394560@uji.es>
2021-10-16 20:33:49 +0200

    ¿Cómo crear un script para tu barra Health?

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -29,8 +29,6 @@ public class RubyController : MonoBehaviour
         animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
-
-        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -87,7 +85,7 @@ public class RubyController : MonoBehaviour
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
     void Launch()
